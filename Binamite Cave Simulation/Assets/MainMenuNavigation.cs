fef1da6f280ddc50b43f96d61d56335ae5600165
MarkRenard <56568308+MarkRenard@users.noreply.github.com>
2019-10-29 20:56:29 -0500

    Main Menu
    The Main Menu exists, shows up, and has 4 buttons. Two of them load stand-in scenes for the two games.

---
+++ Binamite Cave Simulation/Assets/MainMenuNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    public void PlayCavingGame()
+    {
+        print("PlayCavingGame firing!");
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void PlaySearchingGame()
+    {
+        print("PlaySearchingGame firing!");
+
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void Exit()
+    {
+       // todo
+    }
+}
