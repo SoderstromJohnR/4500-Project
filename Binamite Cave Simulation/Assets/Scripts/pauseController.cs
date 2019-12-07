9d8a11a938bc624d98e2e3ff57db591142ca42f6
MarkRenard <56568308+MarkRenard@users.noreply.github.com>
2019-12-07 12:28:07 -0600

    Pause Menu Pauses The Game

--- Binamite Cave Simulation/Assets/Scripts/pauseController.cs
+++ Binamite Cave Simulation/Assets/Scripts/pauseController.cs
@@ -4,51 +4,51 @@ using UnityEngine;
 
 public class pauseController : MonoBehaviour
 {
-    public int pauseSwitch;
-    private int pauseCD;
+    public int pauseSwitch;     // Zero if the game is not paused, one otherwise
+    private int pauseCountDown; // Counts down frames after pausing before new input registers
+
+    const int NUM_FRAMES_TO_WAIT = 15;
+
     // Start is called before the first frame update
     void Start()
     {
-        pauseCD = 0;
-       
+        pauseCountDown = 0;
         pauseSwitch = 0;
-        // zero is inactive
-        // one is active
     }
 
-   
+    // Update is called once per frame
     void Update()
     {
-        if (pauseCD > 0)
+        if (pauseCountDown > 0)
         {
-            pauseCD -= 1;
+            pauseCountDown -= 1;
         }
         GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
         GameObject close = GameObject.Find("closeButton");
+
         //begin pause
         closeController cc = close.GetComponent<closeController>();
-        if (Input.GetKeyDown("p") && pauseSwitch == 0 && pauseCD == 0)
+        if (Input.GetKeyDown("p") && pauseSwitch == 0 && pauseCountDown == 0)
         {
             gameObject.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, 0);
             pauseSwitch = 1;
-            pauseCD = 15;
+            pauseCountDown = NUM_FRAMES_TO_WAIT;
+
+            // Stops time
+            Time.timeScale = 0;
            
         }
+
         //end pause
-        
-        if (cc.closeSwitch == 1 && pauseCD == 0)
+        else if (((Input.GetKeyDown("p") && pauseSwitch == 1) || cc.closeSwitch == 1) && pauseCountDown == 0)
         {
             gameObject.transform.position = new Vector3(100, 100, 0);
             pauseSwitch = 0;
-            pauseCD = 15;
+            pauseCountDown = NUM_FRAMES_TO_WAIT;
+
+            // Restarts time
+            Time.timeScale = 1;
         }
 
-        if (Input.GetKeyDown("p") && pauseSwitch == 1 && pauseCD == 0)
-        {
-            gameObject.transform.position = new Vector3(100, 100, 0);
-            pauseSwitch = 0;
-            pauseCD = 15;
-            
-        }     
     }
 }
